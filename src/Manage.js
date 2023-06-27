260efcaafbb3c40dc9de39325ac399e9b332c619
Asipko <avsipko@gmail.com>
2023-06-27 23:43:52 +0300

    add

--- src/Manage.js
+++ src/Manage.js
@@ -1,17 +1,15 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home'
 import Header from './components/Header'
 
 function Manage() {
   return (
-    <BrowserRouter basename="/predict-app-react">
       <Routes>
         <Route exact path='/a' element={<Home />} />
         <Route path="/t" element={<Header />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import Manage from './Manage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Manage />
+    <BrowserRouter basename="/predict-app-react">
+        <Manage />
+    </BrowserRouter>
 );
