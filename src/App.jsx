c97c04fd9cad638e29f456dc017b6e90de8c9e76
Asipko <avsipko@gmail.com>
2023-07-08 17:06:41 +0300

    add input and button

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+    return (
+      <div className="App">
+        <div className="wrapper">
+            <input
+                type="text"
+                placeholder="Enter something..."
+            />
+            <button className="enter">ENTER</button>
+        </div>
+      </div>
+    );
+  }
+
+export default App
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './Manage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename="predict-app-react">
         <App />
-    </BrowserRouter>
 );
